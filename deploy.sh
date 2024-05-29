6aa40f86cf2a8c5522bd3c124c59593686ffcb16
Dreykovic <amonaaudrey16@gmail.com>
2024-05-29 15:44:57 +0000

    ff

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,17 @@ sudo apt install apache2 apache2-utils libexpat1 python3 python3-dev libapache2-
 cd /var/www/
 git clone https://github.com/Dreykovic/ua-app.git
 cp ua-app/ua.conf /etc/apache2/sites-available/ua.conf
-a2ensite ua.conf 
-a2dissite 000-default.conf 
+
 chown www-data:www-data ua-app/
 chown www-data:www-data ua-app/db.sqlite3 
 chmod 775 ua-app/
 chmod 664 ua-app/db.sqlite3 
-systemctl restart apache2
+apt install python3-venv
+apt install python3-tk
 cd ua-app/
-python3 -m venv venv
-apt install python3.12-venv
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt 
-apt install python3.12-tk
+a2ensite caert.egotransfert.com.conf 
+a2dissite 000-default.conf 
+systemctl restart apache2
