e0210eae9cd5032f219aa40a9b253b847f00697f
Birewa Audrey AMONA <122552210+Dreykovic@users.noreply.github.com>
2024-06-09 14:39:54 +0000

    Update settings.py

--- ua/settings.py
+++ ua/settings.py
@@ -142,9 +142,9 @@ TIME_ZONE = 'UTC'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS= [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = '/home/egofin/ua.egofintech.com/staticfiles'
 # MEDIA_URL='/media/'
 # MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
