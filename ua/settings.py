a24aeb0df49e32d41f931953bac901c36315f4fe
Birewa Audrey AMONA <122552210+Dreykovic@users.noreply.github.com>
2024-06-09 14:05:18 +0000

    Update settings.py

--- ua/settings.py
+++ ua/settings.py
@@ -144,7 +144,7 @@ TIME_ZONE = 'UTC'
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS= [os.path.join(BASE_DIR, 'static')]
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # MEDIA_URL='/media/'
 # MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
